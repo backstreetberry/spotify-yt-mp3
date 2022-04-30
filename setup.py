293ebf1c87e18c89ec5d9e9819e8926914c722b0
Nisarg Parab <93029804+backstreetberry@users.noreply.github.com>
2022-04-30 22:34:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     version = '1.0',
     description='App that gets spotify songs and downloadds the YouTube MP3 version of it',
     author='Nisarg',
-    author_email='nisargparab8691@gmail.com',
+    author_email='abcd@example.com',
     keywords='web flask',
     packages=find_packages(),
     include_package_data=True,
